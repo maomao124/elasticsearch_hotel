abd25060771d73b0e4dd56b16c1c2ad3862936a3
mao <1296193245@qq.com>
2022-06-02 16:15:02 +0800

    elasticsearch_hotel

---
+++ src/test/java/mao/elasticsearch_hotel/hotel/HotelSearchTest.java
@@ -0,0 +1,22 @@
+package mao.elasticsearch_hotel.hotel;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Project name(项目名称)：elasticsearch_hotel
+ * Package(包名): mao.elasticsearch_hotel.hotel
+ * Class(类名): HotelSearchTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/6/2
+ * Time(创建时间)： 16:13
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@SpringBootTest
+public class HotelSearchTest
+{
+
+}
